dc04f26466d79b62664306aa4d36196d0090b6d1
nour-elhabbal <nour.elhabbal9@gmail.com>
2024-06-19 16:56:37 +0300

    Feat: Map implementation

---
+++ src/components/Map.tsx
@@ -0,0 +1,43 @@
+import { useContext, useEffect, useRef } from "react";
+import { Box, Img } from "@chakra-ui/react";
+import { Icon, type Map as LeafletMap } from "leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { ipInfoContext } from "../contexts/ipInfoContext";
+
+const Map = () => {
+  const { info } = useContext(ipInfoContext);
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  const markerIcon = new Icon({
+    iconUrl: "/src/assets/icon-location.svg",
+    iconSize: [34, 40],
+  });
+
+  useEffect(() => {
+    if (mapRef)
+      mapRef.current?.flyTo([info.latitude, info.longitude], 16, {
+        animate: true,
+        duration: 1.5,
+      });
+  }, [info]);
+
+  return (
+    <Box width="100%" height={{ base: "70vh", smallScreen: "65vh" }}>
+      <Box
+        as={MapContainer}
+        ref={mapRef}
+        center={[info.latitude, info.longitude]}
+        zoom={14}
+        scrollWheelZoom
+        zoomControl={false}
+        h="full"
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[info.latitude, info.longitude]} icon={markerIcon} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Map;
